1ecb009c4e141efbdb46d1dab02773feff1e6453
Atanu Maiti <am2z5@mail.umkc.edu>
2018-12-10 16:51:48 -0600

    added add method

--- Tutorial/src/basics/helloWorld.java
+++ Tutorial/src/basics/helloWorld.java
@@ -4,5 +4,9 @@ public class helloWorld {
 	public static void main(String args[]) {
 		System.out.println("hello world!");
 	}
+	
+	public void add (int a , int b) {
+		System.out.println(a+b);
+	}
 
 }
