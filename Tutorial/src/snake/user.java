5a0ca8c0576c807519bc3ed48830c58401898494
Atanu Maiti <am2z5@mail.umkc.edu>
2018-12-11 15:31:04 -0600

    random

--- Tutorial/src/snake/user.java
+++ Tutorial/src/snake/user.java
@@ -2,4 +2,13 @@ package snake;
 
 public class user {
 
+	public void start() {
+		snake cobra = new snake();
+		cobra.move(s);
+		
+	}
+	public static void main(String[] args) {
+		user abdul = new user();
+	}
+	
 }
